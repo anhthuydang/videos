5128c14affe94b537c7a0460cc25339078c2356f
Anh Dang <atd30392@gmail.com>
2021-01-11 15:23:08 -0500

    refactored using hooks

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,8 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
     return (
         <div onClick={() => onVideoSelect(video)} className="video-item item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
+            <img 
+            alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
             <div className="header">
                 {video.snippet.title}
             </div>
