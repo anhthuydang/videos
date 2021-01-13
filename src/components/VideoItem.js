fff328b6e20dda4dfee3b1dbeeaddd96352a4f69
Anh Dang <atd30392@gmail.com>
2021-01-13 14:31:54 -0500

    refactored using custom hook

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './VideoItem.css';
 
 const VideoItem = ({ video, onVideoSelect }) => {
-    return (
-        <div onClick={() => onVideoSelect(video)} className="video-item item">
-            <img 
-            alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
-            <div className="header">
-                {video.snippet.title}
-            </div>
-        </div>
+	return (
+		<div onClick={() => onVideoSelect(video)} className="video-item item">
+			<img
+			alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
+			<div className="header">
+					{video.snippet.title}
+			</div>
+		</div>
 
-    )
+	)
 };
 
 export default VideoItem;
